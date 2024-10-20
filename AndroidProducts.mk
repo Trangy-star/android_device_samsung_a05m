0a571e2a8e5b7d1342b121a54cc32b07ddd34278
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-10-20 05:44:08 +0000

    a05m: derp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a05m.mk
+    $(LOCAL_DIR)/derp_a05m.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a05m-user \
-    lineage_a05m-userdebug \
-    lineage_a05m-eng
+    derp_a05m-user \
+    derp_a05m-userdebug \
+    derp_a05m-eng
--- derp_a05m.mk
+++ derp_a05m.mk
@@ -9,15 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from a05m device
 $(call inherit-product, device/samsung/a05m/device.mk)
 
+DERP_VERSION_APPEND_TIME_OF_DAY=true
+TARGET_USES_MINE_GAPPS := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_SYSTEM_NAME := a05m
 PRODUCT_DEVICE := a05m
-PRODUCT_NAME := lineage_a05m
+PRODUCT_NAME := derp_a05m
 PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := Samsung Galaxy A05
+PRODUCT_MODEL := Galaxy A05
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
