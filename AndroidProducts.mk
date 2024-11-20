3b5d4b05dc41b4c91d82f4a8cce6b0cb25387709
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-11-21 05:24:02 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a05m.mk
+    $(LOCAL_DIR)/afterlife_a05m.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a05m-user \
-    lineage_a05m-userdebug \
-    lineage_a05m-eng
+    afterlife_a05m-userdebug 
