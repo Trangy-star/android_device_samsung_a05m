2104cdb73edd300a8462274556b909bfb0c39993
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-12-10 08:33:23 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a05m.mk
+    $(LOCAL_DIR)/banana_a05m.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a05m-user \
-    lineage_a05m-userdebug \
-    lineage_a05m-eng
+    banana_a05m-user \
+    banana_a05m-userdebug \
+    banana_a05m-eng
